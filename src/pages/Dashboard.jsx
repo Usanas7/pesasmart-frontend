14ee7926ff293ce4560bbacbee31befd00160f8e
usanas7 <c.usanase@alustudent.com>
2026-06-25 16:34:23 +0200

    changed the react-router import line

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
+import { useNavigate, Link } from "react-router-dom";
 const API = import.meta.env.VITE_API_URL;
 
 function Dashboard() {
